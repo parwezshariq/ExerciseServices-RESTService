934869ab591b74602d449a09cd350505fb52faaf
shariqparwez <shariqparwez@outlook.com>
2019-01-30 15:01:34 +0530

    MediaType based request from Client

--- src/main/java/com/shariqparwez/client/ActivityClient.java
+++ src/main/java/com/shariqparwez/client/ActivityClient.java
@@ -3,6 +3,7 @@ package com.shariqparwez.client;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import com.shariqparwez.model.Activity;
 
@@ -17,7 +18,10 @@ public class ActivityClient {
 	public Activity get(String id) {
 		WebTarget target = client.target("http://localhost:8080/exercise-services/webapi/");
 		
-		Activity response = target.path("activities/" + id).request().get(Activity.class);
+		//Activity response = target.path("activities/" + id).request().get(Activity.class);
+		Activity response = target.path("activities/" + id).request(MediaType.APPLICATION_JSON).get(Activity.class);
+		
+		System.out.println(response);
 		
 		return response;
 	
